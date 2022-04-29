dc7e3d6467c8482baffc13cce3c24d577130621e
patrick alexandre <palexandre@live.com>
2022-04-29 19:01:19 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aplicação_teste
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            decimal peso;
+            decimal altura;
+            decimal IMC;
+
+            Console.WriteLine("Insira seu peso: ");
+            peso = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Insira sua altura: ");
+            altura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine(IMC = peso / (altura * altura));
+
+        }
+    }
+}
